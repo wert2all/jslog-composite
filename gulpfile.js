50abbbdfd0ac08eeb4f3e4ee371aa8cb0f6ee7ff
Sergey Shvets <sshvets@magento.com>
2018-02-18 15:13:47 +0200

    added NPM support

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,32 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const del = require('del');
+const babel = require('gulp-babel');
+const minify = require('gulp-minify-fork');
+const concat = require('gulp-concat');
+const packageConfig = require('./package.json');
 
+const config = {
+    build: {
+        folder: 'dist/',
+        filename: packageConfig.name + '.' + packageConfig.version
+    },
+    src: {
+        folder: 'src/'
+    }
+};
+
+// test tasks
 gulp.task('test:clean', (cb) => cb());
 gulp.task('test:check', (cb) => cb());
 
 gulp.task('test:exec:acceptance', cb => cb());
 gulp.task('test:exec:unit', (cb) => cb());
-gulp.task('test:exec:static', () => {
-    // ESLint ignores files with "node_modules" paths.
-    // So, it's best to have gulp ignore the directory as well.
-    // Also, Be sure to return the stream from the task;
-    // Otherwise, the task may end before the stream has finished.
-    return gulp.src(
-        [
-            '**/*.js',
-            '*.js'
-        ])
-    // eslint() attaches the lint output to the "eslint" property
-    // of the file object so it can be used by other modules.
+gulp.task('test:exec:static', () =>
+    gulp.src(['**/*.js', '*.js'])
         .pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
         .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
-        .pipe(eslint.failAfterError());
-});
+        .pipe(eslint.failAfterError()));
 
 gulp.task('test:exec', gulp.parallel('test:exec:unit', 'test:exec:acceptance', 'test:exec:static'));
 gulp.task('test:run', gulp.series(
@@ -34,4 +35,35 @@ gulp.task('test:run', gulp.series(
     'test:check'
 ));
 
-gulp.task('test', gulp.series( 'test:run'));
+gulp.task('test', gulp.series('test:run'));
+// end test tasks
+
+// clean tasks
+gulp.task('clean', () => {
+    return del([config.build.folder + "/**/*"]);
+});
+// end clean tasks
+//build tasks
+gulp.task('build:minify', () => {
+    return gulp.src(config.build.folder + config.build.filename + '.js')
+        .pipe(minify({
+            ext: {
+                src: '.js',
+                min: '.min.js'
+            },
+        }))
+        .pipe(gulp.dest(config.build.folder));
+});
+
+gulp.task('build:compile', () => {
+    return gulp.src(config.src.folder + '**/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(concat(config.build.filename + '.js'))
+        .pipe(gulp.dest(config.build.folder));
+});
+
+gulp.task('build:run', gulp.series('build:compile', 'build:minify'));
+gulp.task('build', gulp.series('clean', 'test', 'build:run'));
+//end build tassks
